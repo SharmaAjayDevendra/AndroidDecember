3970f44d0b2a2c903a6eccbc44212879fe9d756d
Ajay Sharma <ajay90560@gmail.com>
2019-12-15 18:04:20 +0530

    Update MainActivity.java

--- inputfield/MainActivity.java
+++ inputfield/MainActivity.java
@@ -3,6 +3,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     Button loginbutton;
     EditText username, password;
     String output="";
+    CheckBox tandc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,12 +19,15 @@ public class MainActivity extends AppCompatActivity {
         loginbutton=findViewById(R.id.mybutton);
         username=findViewById(R.id.username);
         password=findViewById(R.id.password);
+        tandc=findViewById(R.id.tandc);
         loginbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 output="Your username is: "+username.getText().toString();
                 output+=" Your password is: "+password.getText().toString();
-                Toast.makeText(getBaseContext(), output, Toast.LENGTH_LONG).show();
+                if(tandc.isChecked()){
+                    Toast.makeText(getBaseContext(), output, Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
