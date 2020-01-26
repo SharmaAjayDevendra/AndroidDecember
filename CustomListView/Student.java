33344cb2799892ab33adcb7bc11354acde853cc9
Ajay Sharma <ajay90560@gmail.com>
2020-01-26 17:04:03 +0530

    Create Student.java

---
+++ CustomListView/Student.java
@@ -0,0 +1,10 @@
+package in.autodice.missingpart;
+
+public class Student {
+    String name;
+    String year;
+    public Student(String name, String year){
+        this.name=name;
+        this.year=year;
+    }
+}
