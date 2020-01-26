17793b032d36101fc911df29dcbceb8ed2e5f0fd
Ajay Sharma <ajay90560@gmail.com>
2020-01-26 18:14:28 +0530

    Create CustomAdapter.java

---
+++ CustomListViewWithImage/CustomAdapter.java
@@ -0,0 +1,92 @@
+package in.autodice.missingpart;
+
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class CustomAdapter implements ListAdapter {
+    Context context;
+    ArrayList<Student> data;
+    public  CustomAdapter(Context context, ArrayList<Student> arrayList){
+        this.context=context;
+        this.data=arrayList;
+    }
+
+    @Override
+    public boolean areAllItemsEnabled() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+        return true;
+    }
+
+    @Override
+    public void registerDataSetObserver(DataSetObserver observer) {
+
+    }
+
+    @Override
+    public void unregisterDataSetObserver(DataSetObserver observer) {
+
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Student student=data.get(position);
+        if(convertView==null){
+            LayoutInflater layoutInflater=LayoutInflater.from(context);
+            convertView=layoutInflater.inflate(R.layout.eksinglerow, null);
+            TextView name=convertView.findViewById(R.id.studentname);
+            ImageView image=convertView.findViewById(R.id.studentpic);
+            name.setText(student.name);
+            Picasso.with(context).load(student.pic).into(image);
+        }
+        return convertView;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return data.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
