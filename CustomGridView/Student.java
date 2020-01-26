a3981be46006aff9c05ea9d5ee9e15df8614cfae
Ajay Sharma <ajay90560@gmail.com>
2020-01-26 18:24:11 +0530

    Create Student.java

---
+++ CustomGridView/Student.java
@@ -0,0 +1,10 @@
+package in.autodice.missingpart;
+
+public class Student {
+    String name;
+    String pic;
+    public Student(String name, String pic){
+        this.pic=pic;
+        this.name=name;
+    }
+}
