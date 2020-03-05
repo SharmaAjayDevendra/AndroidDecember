e19f101eb35c26c326c0a8f251245dd070c08f25
Ajay Sharma <ajay90560@gmail.com>
2020-03-05 20:34:21 +0530

    Create Student.java

---
+++ SQLiteDatabase/Student.java
@@ -0,0 +1,11 @@
+package in.autodice.databasedemo;
+public class Student{
+    public String rollNo;
+    public String name;
+    public String year;
+    public Student(String r, String n, String y){
+        this.rollNo=r;
+        this.name=n;
+        this.year=y;
+    }
+}
